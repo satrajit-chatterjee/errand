49a6e876775bf8df25db7602feebf4a0124a93f4
Satrajit Chatterjee <satrajitdchatterjee@gmail.com>
2020-05-19 23:46:45 +0530

    added email formatting

--- functions/index.js
+++ functions/index.js
@@ -3,17 +3,21 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const nodemailer = require('nodemailer');
+// const telegramBot = require('node-telegram-bot-api');
 
 //to make it work you need gmail account
 const gmailEmail = functions.config().gmail.login;
 const gmailPassword = functions.config().gmail.pass;
+// const botToken = functions.config().errand_bot.token;
+// const botChatID = functions.config().errand_bot.global_chat_id;
+// const errandBot = new telegramBot(botToken, {polling: true});
 
 admin.initializeApp();
 
 //creating function for sending emails
 var goMail = function (_data, title) {
 
-    const message = JSON.stringify(_data)
+    var message = JSON.stringify(_data)
 
 //transporter is a way to send your emails
     const transporter = nodemailer.createTransport({
@@ -24,17 +28,26 @@ var goMail = function (_data, title) {
         }
     });
 
+    const email = JSON.parse(message);
+
     // console.log(Object.keys(JSON.parse(message)));
-    if (("order_details" in JSON.parse(message) && title == "Received New Order") || 
-        ("feedback" in JSON.parse(message) && title == "Received New Feedback")){
-            console.log("Yes");
-            // setup email data with unicode symbols
-            //this is how your email are going to look like
+    if (("order_details" in email && title == "Received New Order") || 
+        ("feedback" in email && title == "Received New Feedback")){
+            // console.log("Yes");
+            
+            var to_send = "Customer Name: " + email.fname + " " + email.lname + "\n\n"
+            + "Customer Phone Number: " + email.phno + "\n\n"
+            + "Customer Email ID: " + email.email + "\n\n"
+            + "Customer Delivery Address: " + email.addr + "\n\n"
+            + "Customer Order Details:\n\n\t" + email.order_details;
+
+            console.log(to_send);
+
             const mailOptions = {
                 from: gmailEmail, // sender address
                 to: ['satrajitdchatterjee@gmail.com', "sanchitadchakraborty@gmail.com", "leerrandcompany@gmail.com"], // list of receivers
                 subject: title, // Subject line
-                text: message, // plain text body
+                text: to_send, // plain text body
             };
 
             //this is callback function to return status to firebase console
@@ -46,8 +59,11 @@ var goMail = function (_data, title) {
                 // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
             };
 
-            //call of this function send an email, and return status
+            // //call of this function send an email, and return status
             transporter.sendMail(mailOptions, getDeliveryStatus);
+
+            // post telegram bot message to errand_bot
+            // errandBot.sendMessage(botChatID, to_send);
         }
     
 };
